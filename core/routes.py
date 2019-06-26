72b241e9c2149a367876b4adcfe6b1b23d6a1684
cflb <hjaysimpsoncoorp@gmail.com>
2019-06-26 02:01:22 -0300

    add routes: register and login

--- core/routes.py
+++ core/routes.py
@@ -10,3 +10,11 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register')
+def register():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('about.html')
